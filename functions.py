c83e1cf7281cf8818e0349bcc76ce0e6a833101f
Iridelis40 <134889668+Iridelis40@users.noreply.github.com>
2025-09-14 12:56:17 +0200

    Added description on Translation function

--- functions.py
+++ functions.py
@@ -77,6 +77,7 @@ def Count_Point_Mutations(seq1, seq2):
     return Hamming_distance
 
 def Translation(seq):
+    #Translates  mRNA sequence into a protein string 
     RNA  = ""
     for i in range(0, len(seq), 3):
         codon = seq[i:i+3]
